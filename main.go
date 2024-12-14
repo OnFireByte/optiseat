87326f9d0316ce70b000125700c6eb6c3540b5c5
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2024-12-14 18:01:16 +0700

    fix header

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	http.HandleFunc("OPTIONS /optimize", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		w.WriteHeader(http.StatusNoContent)
 	})
@@ -37,6 +38,7 @@ func main() {
 	http.HandleFunc("POST /optimize", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		var req OptimizeReq
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
